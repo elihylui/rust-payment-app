bb7c51ae24881a5a66c999731b225e40fb62a546
Elizabeth <elihylui@gmail.com>
2022-12-16 21:58:54 +0100

    init

---
+++ src/server.rs
@@ -0,0 +1,15 @@
+use tonic::{transport::Server, Request, Response, Status};
+
+use payments::bitcoin_server::{Bitcoin, BitcoinServer};
+use payments::{BtcPaymentResponse, BtcPaymentRequest};
+
+pub mod payments {
+    tonic::include_proto!("payments");
+}
+
+#[derive(Debug, Default)]
+pub struct BitcoinService{}
+
+fn main() {
+    println!("Hello, world!");
+}
