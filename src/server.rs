16040a2647e279888321ac236bfcdc43d4acafec
Elizabeth <elihylui@gmail.com>
2022-12-17 20:31:10 +0100

    add gPRC server

--- src/server.rs
+++ src/server.rs
@@ -10,6 +10,34 @@ pub mod payments {
 #[derive(Debug, Default)]
 pub struct BitcoinService{}
 
-fn main() {
-    println!("Hello, world!");
+#[tonic::async_trait]
+impl Bitcoin for BitcoinService {
+    async fn send_payment(
+        &self,
+        request: Request<BtcPaymentRequest>,
+    ) -> Result<Response<BtcPaymentResponse>, Status> {
+        println!("Got a request: {:?}", request);
+
+        let req = request.into_inner();
+
+        let reply = BtcPaymentResponse {
+            successful: true,
+            message: format!("Sent {}BTC to {}.", req.amount, req.to_addr).into(),
+        };
+
+        Ok(Response::new(reply))
+    }
+}
+
+[tokio::main]
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let addr = "[::1]:50051".parse()?;
+    let btc_service = BitcoinService::default();
+
+    Server::builder()
+        .add_service(BitcoinServer::new(btc_service))
+        .serve(addr)
+        .await?;
+
+    Ok(())
 }
